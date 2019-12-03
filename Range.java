0705c36765e74667882a1d33a8c1f94fb50dd80a
DeWill404 <mr.irshad.siddique@gmail.com>
2019-12-03 13:13:07 +0530

    First Commit

---
+++ Range.java
@@ -0,0 +1,15 @@
+public class Range
+{
+  public static void main(String[] args) {
+    System.out.println("Interger MIN -> "+ Integer.MIN_VALUE);
+    System.out.println("Interger MAX -> "+ Integer.MAX_VALUE);
+    System.out.println("Float MIN -> "+ Float.MIN_VALUE);
+    System.out.println("Float MAX -> "+ Float.MAX_VALUE);
+    System.out.println("Doubl MIN -> e" +Double.MIN_VALUE);
+    System.out.println("Doubl MAX -> e" +Double.MAX_VALUE);
+    System.out.println("Character MIN -> "+ Character.MIN_VALUE);
+    System.out.println("Character MAX -> "+ Character.MAX_VALUE);
+    System.out.println("Byte MIN -> "+ Byte.MIN_VALUE);
+    System.out.println("Byte MAX -> "+ Byte.MAX_VALUE);
+  }
+}
